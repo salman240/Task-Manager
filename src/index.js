c104ea6b3fd234c2ea7cb29550fc0b2f8f0ae6bf
salman240 <salmanyousaf240@gmail.com>
2020-07-31 15:22:46 +0500

    intro page added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+app.get("/", (req, res) => {
+  res.send(
+    "<center style='display: flex; flex: 1; height: 95vh; align-items: center; justify-content: center;'><h1>Task Manager Rest Api's</h1></center>"
+  );
+});
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+  console.log("Server is up on port " + port);
+});
